8d652fc88c5fd84957a2210675b8fd9ae4d506b9
Nitish-Auriga <nitish.sharma@aurigait.com>
2022-07-26 11:10:01 +0530

    authentication and user module

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->enum('role', ['Admin', 'User']);
+            $table->enum('role', ['Admin', 'User'])->default('User');
             $table->text('stripe_customer_id')->nullable();
             $table->timestamps();
         });
